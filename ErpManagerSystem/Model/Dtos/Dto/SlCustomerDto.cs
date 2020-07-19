6ca56b165a01a4cc8911ef5a307fe3a1758e433a
LiaoXiMing <1435646097@qq.com>
2020-07-19 21:48:43 +0800

    完成查询客户表的操作

---
+++ ErpManagerSystem/Model/Dtos/Dto/SlCustomerDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Model.Dtos.Dto
+{
+    public class SlCustomerDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Postcode { get; set; }
+        public string Address { get; set; }
+        public string Custtel { get; set; }
+        public string Linkman { get; set; }
+        public string Linktel { get; set; }
+        public string Email { get; set; }
+        public bool? Sex { get; set; }
+        public DateTime? Birthday { get; set; }
+        public string Love { get; set; }
+        public string Remark { get; set; }
+    }
+}
